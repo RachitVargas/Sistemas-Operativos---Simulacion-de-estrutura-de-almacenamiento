06a10b2195c8cfe384b364a93929308e43e70d5c
RachitVargas <antony.vargas@ulead.ac.cr>
2021-07-10 22:31:25 -0600

    Primer commit

---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 10 21:45:02 2021
+
+@author: antony.vargasulead.ac.cr
+"""
+
+def main():
+    
+    opcion = 0
+    while opcion != 2:
+        
+        print("Bienvenido al simulador [Estructura de almacenamiento en la nube]")
+        print('Elija una opcion:')
+        
+        print("1. Correr simulacion.")
+        print("2. Salir de la simulacion.")
+        
+        try:
+            opcion = int(input())
+            if opcion == 1:
+                megas_inicial = int(input("Ingrese la cantidad de megas inicial: "))
+                n_dias = int(input("Ingrese la cantidad de dias: "))
+                
+            elif opcion == 2:
+                print("Muchas gracias por usar el simulador.")
+        
+            else:
+                print("Opcion invalida, vuelva a intentarlo.")
+    
+        except:
+            print("Por favor asegurese haber ingresado un valor correcto.")
+            
+
+if __name__ == '__main__':
+    main()
